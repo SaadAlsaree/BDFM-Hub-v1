bf859926d78852ce0657cc54cc4728664c8538e7
saadalsaree <saad.alsaree@hotmail.com>
2026-02-24 19:30:34 +0300

    feat: Add API service files for various features and utilities across the application.

--- frontend-app/src/features/forwarded-correspondence/api/forwarded-correspondence.service.ts
+++ frontend-app/src/features/forwarded-correspondence/api/forwarded-correspondence.service.ts
@@ -2,7 +2,7 @@ import { axiosInstance } from '@/lib/axios';
 import { IResponseList } from '@/types/response';
 import { ForwardedCorrespondenceItem } from '../types/forwarded-correspondence';
 
-const baseUrl = process.env.API_URL || 'http://localhost:5000/BDFM/v1/api';
+const baseUrl = process.env.API_URL || 'http://cm-back.inss.local:5000/BDFM/v1/api';
 
 export const forwardedCorrespondenceService = {
   //Correspondence/GetForwardedCorrespondence
